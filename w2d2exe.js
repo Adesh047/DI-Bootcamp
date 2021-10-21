f9bd2b31439c11eb3058db671a8c7d56fd3a5578
Adesh047 <92357163+Adesh047@users.noreply.github.com>
2021-10-21 23:33:19 +0400

    Add files via upload

---
+++ w2d2exe.js
@@ -0,0 +1,108 @@
+//exe1
+/*let x = 5;
+let y = 2;
+ */
+ /*if(x>y){
+ 	console.log('x is the biggest value')
+ }
+ else if(y<x){
+ 	console.log('x is the biggest value')
+ }
+*/
+
+
+/* let x  = prompt ('value for x');
+console.log (x);
+x = Number(x);
+
+let y  = prompt ('value for y');
+console.log (y);
+x = Number(y);
+
+if (x < y){
+	alert('y is the biggest')
+}
+else if (x>y){
+	alert('x is the biggest')
+}
+else if (x=y){
+	alert('its equal')
+}
+else {
+	alert('error')
+}*/
+
+//exe2
+/*let newDog = "Chihuahua"
+let count = newDog.length; 
+console.log(count);
+ console.log( newDog.toUpperCase());
+ console.log( newDog.toLowerCase());
+
+ let dog2 = "newDog"
+ let count1 =dog2.length
+ console.log(count1);
+
+if (count = count1){
+	alert('I love Chihuahuas, it’s my favorite dog breed')
+}
+else{
+	alert('I dont care, I prefer cats')
+}
+*/
+
+//exe3
+/*let number1 = prompt('entre numder')
+console.log('numder1');
+result = Number(number1);
+if ((numder1%2) == 0){
+	alert('its even')
+}
+else{
+	alert('its odd')
+}*/
+
+//exe4
+/*let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+console.log (users.length);
+result = number(user)
+
+
+console.log ("Lea123 is online")
+console.log ("Lea123 and Princess45 is online")
+console.log ("Lea123,cat&doglovers and Princess45 is online")*/
+
+
+
+//exe gold 
+/*let grade  = prompt ('enter number');
+console.log (grade);
+grade = Number(grade);
+
+if (grade > 90){
+	console.log('A');
+} 
+else if (grade >= 80 || grade <= 90){
+	console.log('B');
+}
+else if (grade >=70 && grade <=80 ){
+	console.log('C')
+}
+else if (grade <= 70){
+	console.log('D')
+}*/
+
+
+let user = 'language';
+
+switch (user) {
+  case 'English':
+    console.log('hello');
+    break;
+    case 'french':
+    console.log('bonjour');
+    break;
+    case 'hebrew':
+    console.log('shalom');
+    default:
+    console.log('1010101010101001111101010100101');
