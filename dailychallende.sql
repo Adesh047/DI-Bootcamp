b9dc35c1697e642699fcb6e7c985b9880bd2263e
Adesh047 <92357163+Adesh047@users.noreply.github.com>
2021-12-01 22:15:08 +0400

    sql relation

---
+++ dailychallende.sql
@@ -0,0 +1,15 @@
+--  create table customer(
+-- customer_id serial primary key,
+-- last_name varchar(50) NOT NULL,
+-- first_name varchar(50) NOT NULL
+-- )
+
+-- INSERT INTO customer (last_name, first_name) VALUES ('Benichou','Marc'),
+-- ('Cohen', 'Yoan'), ('Benichou','Lea'),('Dux', 'Amelia'), ('Grez', 'David'),('Simpson', 'Omer');
+
+--  create table customer_profile(
+-- customer_profile_id serial ,
+--  age numeric,
+-- gender varchar(50) NOT NULL
+   primary key (customer_id)
+-- )
